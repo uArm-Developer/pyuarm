c623a95c50a01572a2fa1918b5828cd9eb29ca11
Alex <alex.jz.tan@gmail.com>
2016-04-25 17:58:07 +0800

    add LICENSE README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# uArm For Python
+
+This is a uArm Library for Python.
+
+## Installation
+
+### Using PYPI *Recommend*
+
+- pip install uarm4py
+
+### Manual Installation
+
+- Download source code
+- python setup.py install
+
+
+## Usage
+```
+import uarm
+uarm.list_uarms()
+```
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='uarm',
     packages=['uarm'],
     author_email='developer@ufactory.cc',
     description='A python library for uArm',
-    url="https://github.com/uArm-Developer/uarm4py",
+    url="https://github.com/UFactory-Alex/uarm4py",
     keywords="uarm4py uarm ufactory",
     install_requires = ['docutils>=0.3','pyserial>=3.0'],
 )
