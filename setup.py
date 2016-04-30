f57bf117e7aebda51aca4171c84378771ed5c27d
Alex <alex.jz.tan@gmail.com>
2016-04-30 14:34:02 +0800

    update github url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='uarm4py',
     packages=['uarm'],
     author_email='developer@ufactory.cc',
     description='A python library for uArm',
-    url="https://github.com/UFactory-Alex/uarm4py",
+    url="https://github.com/uarm-developer/uarm4py",
     keywords="uarm4py uarm ufactory",
     install_requires = ['docutils>=0.3','pyserial>=3.0'],
 )
