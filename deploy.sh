8cba941439da7e07d392b5dff0f4246463261552
Alex Tan <alex.jz.tan@gmail.com>
2016-07-04 12:25:42 +0800

    update setup.py

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-
-python setup.py register -r pypi
-python setup.py sdist upload -r pypi
+#if [[ $# -ne 0 ]] ; then
+if [[ $1 == "upload" ]] ; then
+    python setup.py register -r pypi
+    python setup.py sdist upload -r pypi
+fi
+#fi
 
 pyinstaller -F pyuarm/tools/firmware_helper.py
 pyinstaller -F pyuarm/tools/calibrate.py
 
-
+zip -j dist/release/calibrate_macosx.zip dist/calibrate
+zip -j dist/release/firmware_helper_macosx.zip dist/firmware_helper
--- setup.py
+++ setup.py
@@ -7,27 +7,8 @@ with open(ver_path) as ver_file:
         exec(ver_file.read(), main_ns)
 
 version = main_ns['__version__']
-# print main_ns['__version__']
-#
-#
-#
-# import os
-# import json
-#
+
 long_description = open('README.rst').read()
-#
-# module_dir = os.path.dirname(os.path.abspath(__file__))
-# version_file = os.path.join(module_dir, 'pyuarm','version.json')
-# with open(version_file,'r') as v:
-#     data = json.load(v)
-#     v.close()
-#
-# # pyuarm version
-# version = data['module_version']
-# __version__ = VERSION = version
-#
-# # support version
-# support_versions = data['support_versions']
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
@@ -37,7 +18,7 @@ setup(name='pyuarm',
     author='Joey Song/Alex Tan',
     packages=find_packages(),
     scripts=['pyuarm/tools/firmware_helper.py', 'pyuarm/tools/calibrate.py', 'pyuarm/tools/list_uarms.py'],
-    package_data={'pyuarm.tools': ['avrdude/*'], 'pyuarm':['version.json']},
+    package_data={'pyuarm.tools': ['avrdude/*']},
     include_package_data=True,
     author_email='developer@ufactory.cc',
     url="https://github.com/uarm-developer/pyuarm",
